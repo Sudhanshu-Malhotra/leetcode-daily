fd64801116b155a97de22ff1214e27834d2f950b
Sudhanshu-Malhotra <malhotrasudhanshu128@gmail.com>
2026-09-08 22:01:54 +0530

    Implement countCommas method to count commas in range

---
+++ September/9-Sept-3870-Count-Commas-in-Range.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countCommas(int n) {
+        int count = 0;
+
+        // Numbers from 1000 to n have 1 comma each
+        if (n >= 1000) {
+            count += n - 999;
+        }
+
+        // Numbers from 100000 to n have an additional comma
+        if (n >= 100000) {
+            count += n - 99999;
+        }
+
+        return count;
+    }
+}
